664a93bbf9d1f6c992c98c023f0056157b760abc
Denmey <Denmey98@gmail.com>
2019-10-25 16:32:02 +0300

    Refactored trello utilities class

--- DataModelDBTest/TrelloUtility.py
+++ DataModelDBTest/TrelloUtility.py
@@ -3,17 +3,32 @@ from urllib import request, parse; # For REST-requests
 
 # Implements various functions that help getting data from trello boards.
 class TrelloUtility:
-	# Add wrong ids/keys exceptions?
 
 	def __init__(self, api, token):
 		self.api = api;
 		self.token = token;
 
-	def getBoards(self):
-		url = "https://api.trello.com/1/members/me/boards?fields=name,url&key={}&token={}" \
-			.format(self.api, self.token);
-		reqAnswer = request.urlopen(url).read().decode('utf-8');
-		data = json.loads(reqAnswer);
+	# Auxiliary function for sending requests to Trello.
+	# Returns received data
+	# Add wrong ids/keys exceptions?
+	def sendRequest(self, params, fields=""):
+		keyTokenStr = "key={}&token={}" \
+			.format(self.api, self.token)
+
+		fieldsStr = ""
+		if (fields != ""):
+			fieldsStr = "&fields=" + fields
+
+		url = "https://api.trello.com/1/" + params + "?" + keyTokenStr + fieldsStr
+		data = json.loads(request.urlopen(url).read().decode('utf-8'));
+
+		return data;
+
+	## Getters for boards ##
+
+	def getBoards(self, fields="name,url"):
+		params = "members/me/boards";
+		data = self.sendRequest(params, fields);
 
 		return data;
 
@@ -22,50 +37,48 @@ class TrelloUtility:
 		print("Received boards:");
 		print(prettyData);
 
-	def getBoardActions(self, boardId):
-		url = "https://api.trello.com/1/boards/{}/actions?key={}&token={}" \
-			.format(boardId, self.api, self.token);
-		data = json.loads(request.urlopen(url).read().decode('utf-8'));
+	def getBoardInfo(self, boardId, fields=""):
+		params = "boards/{}".format(boardId);
+		data = self.sendRequest(params, fields);
 
 		return data;
 
-	def getBoardInfo(self, boardId):
-		url = "https://api.trello.com/1/boards/{}?key={}&token={}" \
-			.format(boardId, self.api, self.token);
-		data = json.loads(request.urlopen(url).read().decode('utf-8'));
+	def getBoardMembers(self, boardId, fields=""):
+		params = "boards/{}/members".format(boardId);
+		data = self.sendRequest(params, fields);
 
 		return data;
 
-		prettyData = json.dumps(data, indent=3);
-		print("Info on board", data['name'], ":");
-		print("", prettyData);
-
-	def getListsOnBoard(self, boardId):
-		# Full info on each list
-		# url = "https://api.trello.com/1/boards/{}/lists?key={}&token={}" \
-		# Only id, name and board id
-		url = "https://api.trello.com/1/boards/{}/lists?fields=name,idBoard&key={}&token={}" \
-			.format(boardId, self.api, self.token);
-		data = json.loads(request.urlopen(url).read().decode('utf-8'));
+	def getBoardActions(self, boardId, fields=""):
+		params = "boards/{}/actions".format(boardId);
+		data = self.sendRequest(params, fields);
 
 		return data;
 
-		print(json.dumps(data, indent=3)); # Cyrrilic printing is weird because of encoding, but data is okay.
-
-	def getCardsOnList(self, listId):
-		url = "https://api.trello.com/1/lists/{}/cards?key={}&token={}" \
-			.format(listId, self.api, self.token);
-		data = json.loads(request.urlopen(url).read().decode('utf-8'));
+	def getBoardLists(self, boardId, fields="name"):
+		params = "boards/{}/lists".format(boardId);
+		data = self.sendRequest(params, fields);
 
 		return data;
 
-		print(json.dumps(data, indent=3));
+	## Getters for lists ##
 
-	def getCardInfo(self, cardId):
-		url = "https://api.trello.com/1/cards/{}?key={}&token={}" \
-			.format(cardId, self.api, self.token);
-		data = json.loads(request.urlopen(url).read().decode('utf-8'));
+	def getListCards(self, listId, fields=""):
+		params = "lists/{}/cards".format(listId);
+		data = self.sendRequest(params, fields);
 
 		return data;
 
-		print(json.dumps(data, indent=3));
+	## Getters for cards ##
+
+	def getCardInfo(self, cardId, fields=""):
+		params = "cards/{}".format(cardId);
+		data = self.sendRequest(params, fields);
+
+		return data;
+
+	def getCardLabels(self, cardId, fields=""):
+		params = "cards/{}/labels".format(cardId);
+		data = self.sendRequest(params, fields);
+
+		return data;
