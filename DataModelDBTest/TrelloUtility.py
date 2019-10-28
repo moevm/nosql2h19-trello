10602bd68fd10486960f81590a50ef45f39c7a77
Denmey <Denmey98@gmail.com>
2019-10-28 10:34:12 +0300

    Added functions to trello utilities

--- DataModelDBTest/TrelloUtility.py
+++ DataModelDBTest/TrelloUtility.py
@@ -1,5 +1,6 @@
 import json; # For json parsing and pretty json printing
-from urllib import request, parse; # For REST-requests
+from urllib import request, parse, error; # For REST-requests
+# import urllib;
 
 # Implements various functions that help getting data from trello boards.
 class TrelloUtility:
@@ -11,15 +12,20 @@ class TrelloUtility:
 	# Auxiliary function for sending requests to Trello.
 	# Returns received data
 	# Add wrong ids/keys exceptions?
-	def sendRequest(self, params, fields=""):
+	def sendRequest(self, params, fields="", filter=""):
 		keyTokenStr = "key={}&token={}" \
 			.format(self.api, self.token)
 
 		fieldsStr = ""
 		if (fields != ""):
 			fieldsStr = "&fields=" + fields
+		filterStr = "";
+		if (filter != ""):
+			filterStr = "&filter=" + filter;
 
-		url = "https://api.trello.com/1/" + params + "?" + keyTokenStr + fieldsStr
+		url = "https://api.trello.com/1/" + params + "?" + keyTokenStr + fieldsStr + filterStr;
+		# url = urllib.quote(url);
+		# print(url);
 		data = json.loads(request.urlopen(url).read().decode('utf-8'));
 
 		return data;
@@ -62,6 +68,11 @@ class TrelloUtility:
 		return data;
 
 	## Getters for lists ##
+	def getListInfo(self, listId, fields=""):
+		params = "lists/{}".format(listId);
+		data = self.sendRequest(params, fields);
+
+		return data;
 
 	def getListCards(self, listId, fields=""):
 		params = "lists/{}/cards".format(listId);
@@ -81,4 +92,63 @@ class TrelloUtility:
 		params = "cards/{}/labels".format(cardId);
 		data = self.sendRequest(params, fields);
 
+		return data;
+
+	def getCardMembers(self, cardId, fields=""):
+		params = "cards/{}/members".format(cardId);
+		data = self.sendRequest(params, fields);
+
+		return data;
+
+	def getCardAttachments(self, cardId, fields=""):
+		params = "cards/{}/attachments".format(cardId);
+		data = self.sendRequest(params, fields);
+
+		return data;
+
+	def getCardMoves(self, cardId, fields="data,type,date"):
+		params = "cards/{}/actions".format(cardId);
+		if (fields != "" and 'type' not in fields):
+			fields += ",type";
+		data = self.sendRequest(params, fields);
+		# print(data);
+		filterFunction = lambda x : ((x['type'] == 'updateCard') and ('listAfter' in x['data']));
+		filteredList = list(filter(filterFunction, data));
+
+		return filteredList;
+
+	def getCardComments(self, cardId, fields=""):
+		params = "cards/{}/actions".format(cardId);
+		if (fields != "" and 'type' not in fields):
+			fields += ",type";
+		data = self.sendRequest(params, fields);
+
+		filterFunction = lambda x : ((x['type'] == 'commentCard'));
+		filteredList = list(filter(filterFunction, data));
+
+		return filteredList;
+
+	# Doesn't work.
+	def getCardCreationDate(self, cardId, fields=""):
+		params = "cards/{}/actions".format(cardId);
+		# if (fields != "" and 'type' not in fields):
+		# 	fields += ",type";
+		# data = self.sendRequest(params, fields, "type eq \"createCard\"");
+
+		# filterFunction = lambda x : ((x['type'] == 'commentCard'));
+		# filteredList = list(filter(filterFunction, data));
+
+		return data;
+
+	## Getters for members ##
+	def getMember(self, memberId, fields=""):
+		params = "members/{}".format(memberId);
+		# If there's no such member anymore(deleted account?), 404 exception.
+		try:
+			data = self.sendRequest(params, fields);
+		except error.HTTPError as e:
+			if (e.code == 404):
+				data = None;
+			else:
+				raise;
 		return data;
